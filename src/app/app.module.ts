56d34479e29b12c16d93b55b413ae888392293b2
Luke <mugapedia@gmail.com>
2018-02-27 18:25:34 +0300

    Configure routing for the application, add component to handle 404 requests

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,48 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule }           from '@angular/platform-browser';
+import { NgModule }                from '@angular/core';
+import { Routes, RouterModule }    from '@angular/router'
 
-import { AppComponent } from './app.component';
-import { LoginComponent } from './components/login/login.component';
+
+import { AppComponent }            from './app.component';
+import { LoginComponent }          from './components/login/login.component';
 import { CustomerLookupComponent } from './components/customer-lookup/customer-lookup.component';
-import { SendPinComponent } from './components/send-pin/send-pin.component';
-import { EnterPinComponent } from './components/enter-pin/enter-pin.component';
-import { ConfirmationComponent } from './components/confirmation/confirmation.component';
+import { SendPinComponent }        from './components/send-pin/send-pin.component';
+import { EnterPinComponent }       from './components/enter-pin/enter-pin.component';
+import { ConfirmationComponent }   from './components/confirmation/confirmation.component';
+import { PageNotFoundComponent }   from './components/page-not-found/page-not-found.component';
 
+
+const appRoutes: Routes = [
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: 'customer_lookup',
+    component: CustomerLookupComponent
+  },
+  {
+    path: 'send_pin',
+    component: SendPinComponent
+  },
+  {
+    path: 'enter_pin',
+    component: EnterPinComponent
+  },
+  {
+    path: 'confirm',
+    component: ConfirmationComponent
+  },
+  {
+    path: '',
+    redirectTo: '/login',
+    pathMatch: 'full'
+  },
+  {
+    path: '**',
+    component: PageNotFoundComponent
+  }
+]
 
 @NgModule({
   declarations: [
@@ -16,10 +51,12 @@ import { ConfirmationComponent } from './components/confirmation/confirmation.co
     CustomerLookupComponent,
     SendPinComponent,
     EnterPinComponent,
-    ConfirmationComponent
+    ConfirmationComponent,
+    PageNotFoundComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
