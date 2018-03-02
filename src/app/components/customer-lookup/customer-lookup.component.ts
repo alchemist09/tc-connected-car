81c71f71d10e67e2d4816bd4c0d02d07b6c7dc9e
Luke <mugapedia@gmail.com>
2018-03-02 23:41:12 +0300

    Adds Send PIN component, updates placeholder values of customer lookup component

--- src/app/components/customer-lookup/customer-lookup.component.ts
+++ src/app/components/customer-lookup/customer-lookup.component.ts
@@ -1,7 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup,
+         FormControl,
+         Validators
+}                            from '@angular/forms';
 
 @Component({
-  selector: 'app-customer-lookup',
+  selector: 'customer-lookup',
   templateUrl: './customer-lookup.component.html',
   styleUrls: ['./customer-lookup.component.scss']
 })
@@ -9,7 +13,40 @@ export class CustomerLookupComponent implements OnInit {
 
   constructor() { }
 
+  customerLookupForm: FormGroup;
+  phoneNumber: FormControl;
+  email: FormControl;
+
   ngOnInit() {
+    this.createFormControls();
+    this.createForm();
+  }
+
+  createFormControls() {
+    this.phoneNumber = new FormControl('', [
+      Validators.required,
+      Validators.pattern(/^[\d]{3}\-[\d]{3}(\-[\d]{4})?$/)
+    ]);
+    this.email = new FormControl('', [
+      Validators.required,
+      Validators.pattern(/^[\w_.\-]+\@[\w]+\.[\w]{2,6}$/)
+    ]);
+  }
+
+  createForm() {
+    this.customerLookupForm = new FormGroup({
+      phoneNumber : this.phoneNumber,
+      email :this.email
+    });
+  }
+
+  lookupCustomer() {
+    console.log("Customer Lookup form submitted");
+    this.customerLookupForm.reset();
+  }
+
+  resetForm() {
+    this.customerLookupForm.reset();
   }
 
 }
--- src/app/components/send-pin/send-pin.component.ts
+++ src/app/components/send-pin/send-pin.component.ts
@@ -1,7 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  FormGroup,
+  FormControl,
+  Validators
+}                            from '@angular/forms';
 
 @Component({
-  selector: 'app-send-pin',
+  selector: 'send-pin',
   templateUrl: './send-pin.component.html',
   styleUrls: ['./send-pin.component.scss']
 })
@@ -9,7 +14,40 @@ export class SendPinComponent implements OnInit {
 
   constructor() { }
 
+  sendPinForm: FormGroup;
+  firstName: FormControl;
+  lastName: FormControl;
+
   ngOnInit() {
+    this.createFormControls();
+    this.createForm()
+  }
+
+  createFormControls() {
+    this.firstName = new FormControl('', [
+      Validators.required,
+      Validators.pattern(/^[\w]{2,}$/)
+    ]);
+    this.lastName = new FormControl('', [
+      Validators.required,
+      Validators.pattern(/^[\w_.\-]{2,}$/)
+    ]);
+  }
+
+  createForm() {
+    this.sendPinForm = new FormGroup({
+      firstName : this.firstName,
+      lastName :this.lastName
+    });
+  }
+
+  sendPin() {
+    console.log("Send Pin form submitted");
+    this.sendPinForm.reset();
+  }
+
+  resetForm() {
+    this.sendPinForm.reset();
   }
 
 }
